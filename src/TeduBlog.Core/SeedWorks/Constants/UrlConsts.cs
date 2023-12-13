9dc63c02a45c06ac80b8d5d4638a860738325cd0
ThanhDuongDT <duongminhthanhdt@gmail.com>
2023-12-13 10:54:07 +0700

    Xây dựng trang chủ và Hiển thị danh sách bài viết theo danh mục

---
+++ src/TeduBlog.Core/SeedWorks/Constants/UrlConsts.cs
@@ -0,0 +1,21 @@
+﻿namespace TeduBlog.Core.SeedWorks.Constants
+{
+    public static class UrlConsts
+    {
+        public static string Posts = "/posts";
+        public static string Home = "/";
+        public static string About = "/about";
+        public static string Contact = "/contact";
+        public static string PostByCategorySlug = "/posts/{0}";
+        public static string PostDetails = "/posts/{0}";
+        public static string PostByTagSlug = "/tag/{0}";
+        public static string Login = "/login";
+        public static string Register = "/register";
+        public static string Profile = "/profile";
+        public static string Author = "/author/{0}";
+
+        public static string ChangePassword = "/change-password";
+
+
+    }
+}
--- src/TeduBlog.WebApp/Components/NavigationViewComponent.cs
+++ src/TeduBlog.WebApp/Components/NavigationViewComponent.cs
@@ -1,12 +1,32 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore.Migrations;
+using TeduBlog.Core.Repositories;
+using TeduBlog.Core.SeedWorks;
+using TeduBlog.WebApp.Models;
 
 namespace TeduBlog.WebApp.Components
 {
     public class NavigationViewComponent : ViewComponent
     {
+        private readonly IUnitOfWork _uniOfWork;
+        public NavigationViewComponent(IUnitOfWork unitOfWork)
+        {
+            _uniOfWork = unitOfWork;
+        }
         public async Task<IViewComponentResult> InvokeAsync()
         {
-            return View();
+            var model = await _uniOfWork.PostCategories.GetAllAsync();
+            var navItems = model.Select(x => new NavigationItemViewModel()
+            {
+                Slug = x.Slug,
+                Name = x.Name,
+                Children = model.Where(x => x.ParentId == x.Id).Select(i => new NavigationItemViewModel() 
+                {
+                    Name = x.Name,
+                    Slug = x.Slug
+                }).ToList()
+            }).ToList() ;
+            return View(navItems);
         }
     }
 }
