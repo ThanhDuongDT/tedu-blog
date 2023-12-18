523b88997ed51377e577d9eaf42cb880f7a7229c
ThanhDuongDT <duongminhthanhdt@gmail.com>
2023-12-18 08:15:51 +0700

    Đăng nhập

--- src/TeduBlog.WebApp/Controllers/AuthController.cs
+++ src/TeduBlog.WebApp/Controllers/AuthController.cs
@@ -1,7 +1,11 @@
-﻿using Microsoft.AspNetCore.Authorization;
+﻿using MediatR;
+using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore.Metadata.Internal;
 using TeduBlog.Core.Domain.Identity;
+using TeduBlog.Core.Events.LoginSuccessed;
+using TeduBlog.Core.Events.RegisterSuccessed;
 using TeduBlog.Core.SeedWorks.Constants;
 using TeduBlog.WebApp.Models;
 
@@ -11,13 +15,15 @@ namespace TeduBlog.WebApp.Controllers
     {
         private readonly UserManager<AppUser> _userManager;
         private readonly SignInManager<AppUser> _signInManager;
-        public AuthController(UserManager<AppUser> userManager, SignInManager<AppUser> signInManager)
+        private readonly IMediator _mediator;
+        public AuthController(UserManager<AppUser> userManager, SignInManager<AppUser> signInManager, IMediator mediator)
         {
             _userManager = userManager;
             _signInManager = signInManager;
+            _mediator = mediator;
         }
         [HttpGet]
-        [Route("/register")]
+        [Route("register")]
         public IActionResult Register()
         {
             return View();
@@ -43,6 +49,7 @@ namespace TeduBlog.WebApp.Controllers
             {
                 var user = await _userManager.FindByNameAsync(model.Email);
                 await _signInManager.SignInAsync(user, true);
+                await _mediator.Publish(new RegisterSuccessedEvent(model.Email));
                 return Redirect(UrlConsts.Profile);
             }
             else
@@ -51,6 +58,42 @@ namespace TeduBlog.WebApp.Controllers
                 {
                     ModelState.AddModelError(string.Empty, error.Description);
                 }
+            }
+            return View();
+        }
+        [HttpGet]
+        [Route("login")]
+        public IActionResult Login()
+        {
+            return View();
+        }
+        [HttpPost]
+        [AllowAnonymous]
+        [ValidateAntiForgeryToken]
+        [Route("login")]
+        public async Task<IActionResult> Login([FromForm] LoginViewModel model)
+        {
+            if (!ModelState.IsValid)
+            {
+                return View();
+            }
+            var user = await _userManager.FindByNameAsync(model.Email);
+            if (user == null)
+            {
+                ModelState.AddModelError(string.Empty, "Email not found!");
+                return View();
+            }
+            var result = await _signInManager.PasswordSignInAsync(user, model.Password, true, true);
+            if (result.Succeeded)
+            {
+                await _signInManager.SignInAsync(user, false);
+                await _mediator.Publish(new LoginSuccessedEvent(model.Email));
+
+                return Redirect(UrlConsts.Profile);
+            }
+            else
+            {
+                ModelState.AddModelError(string.Empty, "Login failed");
             }
             return View();
         }
